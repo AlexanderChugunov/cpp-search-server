86bb3c84df85a585b5eb4bfe00258ed010b7fa90
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-06-20 11:04:36 +0300

    Delete document.cpp

--- document.cpp
+++
@@ -1,7 +0,0 @@
-#include"document.h"
-Document::Document() = default;
-Document::Document(int id, double relevance, int rating){
-Document::id = id;
-Document::relevance = relevance;
-Document::rating = rating;
-}
