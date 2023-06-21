f4a1cafa0a47bb34bea62b69e2d1e638dd7c88b0
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2023-06-21 15:37:49 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -12,11 +12,17 @@ public:
     }
     // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
+        return result;
+    }
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
 
-    int GetNoResultRequests() const;
+    int GetNoResultRequests() const {
+        return no_results_requests_;
+    }
 private:
     struct QueryResult {
         uint64_t timestamp;
@@ -30,9 +36,4 @@ private:
 
     void AddRequest(int results_num);
 };
-template <typename DocumentPredicate> 
-std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-    AddRequest(result.size());
-    return result;
-}
+
